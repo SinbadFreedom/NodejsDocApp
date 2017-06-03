5a3f1dfcc0b5795d8d6380e255b664b32fa91aff
SinbadFreedom <151229152@qq.com>
2017-06-04 01:38:43 +0800

    add singnature release, change enableProguardInReleaseBuilds true

---
+++ index.android.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    WebView,
+    StatusBar,
+    StyleSheet,
+    View,
+    BackHandler
+} from 'react-native';
+
+const styles = StyleSheet.create({
+                                     container: {
+                                         flex: 1,
+                                         backgroundColor: '#1FB9FF',
+                                         borderWidth: 0
+                                     },
+                                 });
+
+const WEB_VIEW_REF = "webView";
+
+class NodejsDocApp extends Component {
+
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.backHandler);
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.backHandler);
+    }
+
+    backHandler = () => {
+        this.refs[WEB_VIEW_REF].goBack();
+        return true;
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar hidden={true}></StatusBar>
+                <WebView
+                    ref={WEB_VIEW_REF}
+                    source={ {uri : 'http://139.59.45.254/html_nodejs/'}}
+                />
+            </View>
+        );
+    }
+}
+
+
+AppRegistry.registerComponent('NodejsDocApp', () => NodejsDocApp);
